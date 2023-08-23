eea408654c2df21c3f51652a107719f1ac4e2bd0
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-08-23 20:46:29 -0300

    made post to API, started with createAsyncThunk function

--- src/components/myBookList.js
+++ src/components/myBookList.js
@@ -1,9 +1,12 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Book from './book';
 import '../styles/bookList.css';
+import { fetchBooks } from '../redux/books/bookSlice';
 
 const MyBookList = () => {
   const { booksArr } = useSelector((store) => store.bookStore);
+  const dispatch = useDispatch();
+  dispatch(fetchBooks());
   return (
     <ul className="booksList">
       {booksArr.map((el) => <Book key={el.item_id} item={el} />)}
--- src/redux/books/bookSlice.js
+++ src/redux/books/bookSlice.js
@@ -1,25 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/z5kjKgi8wFsiuCStbu32/books';
+
+export const fetchBooks = createAsyncThunk(
+  'books/booksBooksSlice',
+  async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  },
+);
 
 const initialState = {
   booksArr: [
-    {
-      item_id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
+    // {
+    //   item_id: 'item1',
+    //   title: 'The Great Gatsby',
+    //   author: 'John Smith',
+    //   category: 'Fiction',
+    // },
+    // {
+    //   item_id: 'item2',
+    //   title: 'Anna Karenina',
+    //   author: 'Leo Tolstoy',
+    //   category: 'Fiction',
+    // },
+    // {
+    //   item_id: 'item3',
+    //   title: 'The Selfish Gene',
+    //   author: 'Richard Dawkins',
+    //   category: 'Nonfiction',
+    // },
   ],
 };
 
@@ -41,6 +52,45 @@ const bookSlice = createSlice({
     },
   },
 });
+
+// const myId = 'z5kjKgi8wFsiuCStbu32';
+// const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/z5kjKgi8wFsiuCStbu32/books';
+// fetch(url, {
+//   method: 'GET',
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then((response) => (response.text()))
+//   .then((text) => console.log(text));
+
+// fetch(url, {
+//   method: 'POST',
+//   body: JSON.stringify({
+//     item_id: 12132152,
+//     title: 'stormligth Archive',
+//     author: 'Branderson',
+//     category: 'Medieval Fantasy',
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then((res) => res.text())
+//   .then((text) => console.log(text));
+
+// const deleteId = `${url}/12132152`;
+// fetch(deleteId, {
+//   method: 'DELETE',
+//   body: JSON.stringify({
+//     item_id: '12132152',
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then((res) => res.text())
+//   .then((text) => console.log(text));
 
 export const { addBook, removeBook } = bookSlice.actions;
 export default bookSlice.reducer;
