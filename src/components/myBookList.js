9e8beeb1dd47916702fbdd2a208590f6168de6fd
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-08-23 21:45:42 -0300

    can display books

--- src/components/myBookList.js
+++ src/components/myBookList.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-// import Book from './book';
+import Book from './book';
 import '../styles/bookList.css';
 import { fetchBooks } from '../redux/books/bookSlice';
 
@@ -15,11 +15,14 @@ const MyBookList = () => {
     );
   }
   if (status === 'succeeded') {
+    // Object.keys(booksArr).forEach((key) => console.log(booksArr[key][0]));
+    Object.keys(booksArr).map((key) => console.log(booksArr[key]));
     return (
-      // <ul className="booksList">
-      //   {booksArr.map((el) => <Book key={el.item_id} item={el} />)}
-      // </ul>
-      Object.keys(booksArr).forEach((key) => console.log(booksArr[key]))
+      <>
+        <ul className="booksList">
+          {Object.keys(booksArr).map((key) => <Book key={key} item={booksArr[key][0]} />)}
+        </ul>
+      </>
     );
   }
   return (<p>aaaaaa</p>);
@@ -29,3 +32,5 @@ export default MyBookList;
 // <ul className="booksList">
 //   {booksArr.map((el) => <Book key={el.item_id} item={el} />)}
 // </ul>
+
+// {Object.keys(booksArr).forEach((oKey) => <Book key={oKey} item={booksArr[oKey][0]} />)}
