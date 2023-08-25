d5e5946e2fc70d421e9e762675462bfa97bc2083
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-08-25 16:48:20 -0300

    started with form styles

--- src/components/addBook.js
+++ src/components/addBook.js
@@ -1,41 +1,42 @@
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { createBook } from '../redux/books/bookSlice';
+import '../styles/form.css';
 
 const AddBookForm = () => {
   const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
-  const [category, setCategory] = useState('');
   const handleChange = (e) => {
     setTitle(e.target.value);
   };
   const handleChange2 = (e) => {
     setAuthor(e.target.value);
   };
-  const handleChange3 = (e) => {
-    setCategory(e.target.value);
-  };
   return (
-    <form>
-      <input placeholder="Add Title" value={title} onChange={handleChange} />
-      <input placeholder="Add Author" value={author} onChange={handleChange2} />
-      <input placeholder="Add Category" value={category} onChange={handleChange3} />
-      <button
-        type="button"
-        onClick={() => {
-          const newBook = {
-            item_id: `${Date.now()}a`,
-            title,
-            author,
-            category,
-          };
-          dispatch(createBook(newBook));
-        }}
-      >
-        Add Book
-      </button>
-    </form>
+    <section className="formSection">
+      <h3 className="addNewBook">ADD NEW BOOK</h3>
+      <form className="form">
+        <input className="titleInput" placeholder="Add Title" value={title} onChange={handleChange} />
+        <input className="authorinput" placeholder="Add Author" value={author} onChange={handleChange2} />
+        <button
+          className="addBookBtn"
+          type="button"
+          onClick={() => {
+            const newBook = {
+              item_id: `${Date.now()}a`,
+              title,
+              author,
+              category: 'fantasy',
+            };
+            dispatch(createBook(newBook));
+          }}
+        >
+          Add Book
+        </button>
+      </form>
+
+    </section>
   );
 };
 
